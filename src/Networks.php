1f0f4c1d1b09c6ed34e157dce07a5f898553104f
infinex-exchange <admin@infinex.cc>
2023-10-17 18:11:01 +0200

    Refactor 4 and 5

--- src/Networks.php
+++ src/Networks.php
@@ -64,6 +64,104 @@ class Networks {
     }
     
     public function getNetworks($body) {
+        if(isset($body['enabled']) && !is_bool($body['enabled']))
+            throw new Error('VALIDATION_ERROR', 'enabled');
+        
+        $pag = new Pagination\Offset(50, 500, $query);
+    
+        $task = [];
+        
+        $sql = 'SELECT netid,
+                       description,
+                       icon_url,
+                       native_assetid,
+                       confirms_target,
+                       enabled,
+                       memo_name,
+                       native_qr_format,
+                       token_qr_format,
+                       deposit_warning,
+                       withdrawal_warning,
+                       block_deposits_msg,
+                       block_withdrawals_msg
+                FROM networks
+                WHERE 1=1';
+        
+        if(@$body['enabled'])
+            $sql .= ' AND networks.enabled = TRUE';
+        
+        $sql .= ' ORDER BY netid ASC'
+             . $pag -> sql();
+        
+        $q = $this -> pdo -> prepare($sql);
+        $q -> execute($task);
+        
+        $networks = [];
+        
+        while($row = $q -> fetch()) {
+            if($pag -> iter()) break;
+            $networks[] = $this -> rtrNetwork($row);
+        }
+        
+        return [
+            'networks' => $networks,
+            'more' => $pag -> more
+        ];
+    }
+    
+    public function getNetwork($netid) {
+        if(isset($body['netid']) && isset($body['symbol']))
+            throw new Error('ARGUMENTS_CONFLICT', 'Both netid and symbol are set');
+        else if(isset($body['netid'])) {
+            if(!$this -> validateNetworkSymbol($body['netid']))
+                throw new Error('VALIDATION_ERROR', 'netid');
+            $dispNet = $body['netid'];
+        }
+        else if(isset($body['symbol'])) {
+            if(!$this -> validateNetworkSymbol($body['symbol']))
+                throw new Error('VALIDATION_ERROR', 'symbol', 400);
+            $dispNet = $body['symbol'];
+        }
+        
+        $task = [];
+        
+        $sql = 'SELECT netid,
+                       description,
+                       icon_url,
+                       native_assetid,
+                       confirms_target,
+                       enabled,
+                       memo_name,
+                       native_qr_format,
+                       token_qr_format,
+                       deposit_warning,
+                       withdrawal_warning,
+                       block_deposits_msg,
+                       block_withdrawals_msg
+                FROM networks';
+        
+        if(isset($body['netid'])) {
+            $task[':netid'] = $body['netid'];
+            $sql .= ' WHERE netid = :netid';
+            $dispNet = $body['netid'];
+        }
+        else {
+            $task[':symbol'] = $body['symbol'];
+            $sql .= ' WHERE symbol = :symbol';
+            $dispNet = $body['symbol'];
+        }
+        
+        $q = $this -> pdo -> prepare($sql);
+        $q -> execute($task);
+        $row = $q -> fetch();
+        
+        if(!$row)
+            throw new Error('NOT_FOUND', 'Network '.$dispNet.' not found', 404);
+        
+        return $this -> rtrNetwork($row);
+    }
+    
+    /*public function getNetworks($body) {
         if(isset($body['assetid']) && !is_string($body['assetid']))
             throw new Error('VALIDATION_ERROR', 'assetid');
         if(isset($body['enabled']) && !is_bool($body['enabled']))
@@ -141,14 +239,24 @@ class Networks {
         $row = $q -> fetch();
         
         return $this -> rowToNetworkItem($row);
-    }
+    }*/
     
-    public function rowToNetworkItem($row) {
+    private function ptpNetwork($row) {
         return [
+            'netid' => $row['netid'],
             'symbol' => $row['netid'],
             'name' => $row['description'],
             'iconUrl' => $row['icon_url'],
-            'memoName' => $row['memo_name']
+            'nativeAssetid' => $row['native_assetid'],
+            'confirmTarget' => $row['confirms_target'],
+            'enabled' => $row['enabled'],
+            'memoName' => $row['memo_name'],
+            'qrFormatNative' => $row['native_qr_format'],
+            'qrFormatToken' => $row['token_qr_format'],
+            'depositWarning' => $row['deposit_warning'],
+            'withdrawalWarning' => $row['withdrawal_warning'],
+            'blockDepositsMsg' => $row['block_deposits_msg'],
+            'blockWithdrawalsMsg' => $row['block_withdrawals_msg']
         ];
     }
     
